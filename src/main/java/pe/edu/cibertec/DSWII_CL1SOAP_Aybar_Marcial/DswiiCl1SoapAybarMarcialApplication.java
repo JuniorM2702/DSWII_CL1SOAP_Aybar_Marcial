f945e49400eefadfdfeb6153fe2f81f3faecf42c
JuniorM2702 <Junior_aybar27@hotmail.com>
2024-06-08 14:13:28 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_Aybar_Marcial/DswiiCl1SoapAybarMarcialApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_Aybar_Marcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapAybarMarcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapAybarMarcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL1SOAP_Aybar_Marcial/DswiiCl1SoapAybarMarcialApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_Aybar_Marcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapAybarMarcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
